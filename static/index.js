67e4cad8d1086455adaebab439448c95ae5cfaa8
pavanpej <pavanpej@gmail.com>
2019-05-10 20:19:02 +0530

    add ajax form submit and img preview

--- static/index.js
+++ static/index.js
@@ -1,1 +1,88 @@
-console.log("Inside");
+$(document).ready(() => {
+  console.log("JS Works!");
+
+  const loaderId = "#loader";
+  const inputImgId = "#img";
+  const outputImgId = "#outputImg";
+  const classOutputId = "#classOutput";
+  const classOutputWrapperId = "#classOutputWrapper";
+  const predictButtonId = "#predictButton";
+  const classOutputElement = document.querySelector(classOutputId);
+  const classOutputWrapperElement = document.querySelector(
+    classOutputWrapperId
+  );
+
+  // for sumbmitting the form
+  $("form").submit(e => {
+    e.preventDefault();
+
+    // disable the button after submitting and put in the loader
+    $(predictButtonId).attr("disabled", true);
+    $(predictButtonId).text("Submitted");
+    $(classOutputId).html(`
+    Predicting
+    <div class="loader-wrapper d-inline">
+      <div id="loader" class="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+    `);
+
+    // load the form data
+    const formData = new FormData($("form").get(0));
+
+    $.ajax({
+      url: "/predict",
+      type: "POST",
+      data: formData,
+      processData: false,
+      contentType: false,
+      success: data => {
+        console.debug("Data Recieved:");
+        console.dir(data);
+        if (data.success == true) {
+          $(classOutputId).html(data.class);
+          $(predictButtonId).removeAttr("disabled");
+          switch (data.predictions[0]) {
+            case 0: // bacterial class
+            case 2: // viral class
+              classOutputElement.classList = "bg-custom-danger";
+              break;
+            case 1: // normal class
+              classOutputElement.classList = "bg-success";
+              break;
+            default:
+              classOutputElement.classList = "bg-info";
+          }
+        }
+      }
+    });
+  });
+
+  $(inputImgId).on("change", e => {
+    const input = e.target;
+
+    const fileName = $(input)
+      //get the file name
+      .val()
+      //replace the "Choose a file" label
+      .replace("C:\\fakepath\\", " ");
+
+    // display the file name
+    $(input)
+      .next(".custom-file-label")
+      .html(fileName);
+
+    // display the image
+    if (input.files && input.files[0]) {
+      let reader = new FileReader();
+      reader.onload = e => {
+        $(outputImgId).attr("src", e.target.result);
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  });
+});
