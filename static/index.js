2e57c88dbadba2c80ff4859ff33e05f5c97e96b6
pavanpej <pavanpej@gmail.com>
2019-05-13 21:49:59 +0530

    reconfig for new backend, fix btn and color issue

--- static/index.js
+++ static/index.js
@@ -19,6 +19,7 @@ $(document).ready(() => {
     // disable the button after submitting and put in the loader
     $(predictButtonId).attr("disabled", true);
     $(predictButtonId).text("Submitted");
+
     $(classOutputId).html(`
     Predicting
     <div class="loader-wrapper d-inline">
@@ -40,37 +41,45 @@ $(document).ready(() => {
       data: formData,
       processData: false,
       contentType: false,
+      beforeSend: () => {
+        console.log("In beforeSend()");
+        classOutputElement.classList = "bg-info";
+      },
       success: data => {
         console.debug("Data Recieved:");
         console.dir(data);
         if (data.success == true) {
           $(classOutputId).html(data.class);
           $(predictButtonId).removeAttr("disabled");
-          switch (data.predictions[0]) {
-            case 0: // bacterial class
-            case 2: // viral class
-              classOutputElement.classList = "bg-custom-danger";
-              break;
-            case 1: // normal class
+          $(predictButtonId).html("Submit");
+
+          switch (data.prediction) {
+            case 0: // Normal Class
               classOutputElement.classList = "bg-success";
+              break;
+            case 1: // Bacterial class
+            case 2: // Viral class
+              classOutputElement.classList = "bg-custom-danger";
               break;
             default:
               classOutputElement.classList = "bg-info";
           }
+
+        } else {
+          console.error("Some Error Occurred at Client");
         }
+      },
+      error: xhr => {
+        console.error(xhr);
       }
     });
   });
 
   $(inputImgId).on("change", e => {
     const input = e.target;
-
     const fileName = $(input)
-      //get the file name
-      .val()
-      //replace the "Choose a file" label
-      .replace("C:\\fakepath\\", " ");
-
+      .val() //get the file name
+      .replace("C:\\fakepath\\", " "); //replace the "Choose a file" label
     // display the file name
     $(input)
       .next(".custom-file-label")
